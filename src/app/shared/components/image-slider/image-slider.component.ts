ab12f8d922d1e4f15b320db16aeb09f47c4b5fc4
Edward Josue <Edward_Developer@outlook.com>
2024-04-14 22:03:29 -0700

    feat add swiper js options to component

--- src/app/shared/components/image-slider/image-slider.component.ts
+++ src/app/shared/components/image-slider/image-slider.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component } from '@angular/core';
-import { register } from 'swiper/element/bundle';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { SwiperContainer, register } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types';
 // register Swiper custom elements
 register();
 
@@ -13,6 +14,26 @@ register();
   styleUrl: './image-slider.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ImageSliderComponent {
+export class ImageSliderComponent implements OnInit {
+
+  swiperElement = signal<SwiperContainer | null>(null);
+
+  ngOnInit(): void {
+    const swiperElementConstructor = document.querySelector('swiper-container');
+    const swiperOptions: SwiperOptions = {
+      pagination: true,
+      navigation: {
+        enabled: true
+      },
+      autoplay: {
+        delay: 3000
+      }
+    }
+
+    Object.assign(swiperElementConstructor!, swiperOptions);
+    this.swiperElement.set(swiperElementConstructor as SwiperContainer);
+    this.swiperElement()?.initialize();
+    
+  }
 
 }
